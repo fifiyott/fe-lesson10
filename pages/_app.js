b93bd99a7954f210701ed1bf0059f1f9f2348bb1
fifiyott <fifi.lutfiazahra@gmail.com>
2021-09-18 12:52:53 +0700

    1st commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,25 @@
+import Header from '../components/header'
 import '../styles/globals.css'
+import { Provider } from 'react-redux';
+import store from '../components/store/store';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+
+
+
+  return (
+    <Provider store={store}>
+    <div>
+      <Header></Header>
+      
+    
+  
+      <Component {...pageProps} />
+    </div>
+    </Provider>
+   
+  )
+  
 }
 
 export default MyApp
---
+++ pages/counterpage.js
@@ -0,0 +1,13 @@
+import React, { useState } from 'react';
+import Counter from '../components/counter/counter'; 
+
+function CounterPage() {
+    return (
+       <div>
+            <Counter></Counter>
+       </div>
+         )
+
+}
+
+export default CounterPage
